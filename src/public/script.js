1c5f58e93c41214200e1976424b52b42847d94f2
SherlockPE <fabriciolopezreyes@gmail.com>
2025-12-02 12:17:09 +0100

    add some styles

--- src/public/script.js
+++ src/public/script.js
@@ -1,1 +1,17 @@
-alert("Hola Globant");
+const container_grid = document.querySelector('.grid-container');
+
+function createStructure()
+{
+    for (let i = 0; i < 16; i++)
+    {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        cell.textContent = i + 1;
+        container_grid.appendChild(cell);
+    }
+}
+
+if (!container_grid)
+    console.log("error getting the grid-container");
+else
+    createStructure();
